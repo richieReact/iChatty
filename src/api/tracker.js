df41f8e4da9b68cec0b0f7c878dec2358396ad6f
Richard Channell <richardchannelljr@gmail.com>
2021-04-01 23:54:08 -0400

    Bug fixed! Signup working!

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const instance = axios.create({
-  baseURL: 'http://e8db71cfccc2.ngrok.io'
+  baseURL: 'http://b0fc32cdfc8b.ngrok.io'
 })
 // 192.168.0.17
 
