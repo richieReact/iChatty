d7295b7db8a4f62c3e73478f6f16c79f881ab5f6
Richard Channell <richardchannelljr@gmail.com>
2021-11-05 01:49:19 -0400

    idk lol

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -3,13 +3,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const instance = axios.create({
   // it works!!!!
-  baseURL: 'http://192.168.0.6:8000'
+  baseURL: 'http://192.168.0.6:8000',
 })
 // http://192.168.0.17:8000 might work here, try it sometime, ngrok otherwise
 
 instance.interceptors.request.use(
   async (config) => {
     const token = await AsyncStorage.getItem('token')
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
